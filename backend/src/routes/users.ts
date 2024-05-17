90ac9592731c1e5fd3f32403b6de612f4fdebeef
ColinChan520 <104440913+ColinChan520@users.noreply.github.com>
2024-05-17 14:13:25 +0930

    Add user registration route

---
+++ backend/src/routes/users.ts
@@ -0,0 +1,22 @@
+import express, {Request, Response} from 'express';
+import User from '../models/user';
+
+const router = express.Router();
+
+router.post("/register", async (req: Request, res: Response) => {
+    try{
+        let user = await User.findOne({
+            email: req.body.email,
+        });
+
+        if (user) {
+            return res.status(400).json({message: "User already exists"});
+        }
+
+        user = new User(req.body);
+        await user.save();
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message: "Server error"});
+    }
+});
