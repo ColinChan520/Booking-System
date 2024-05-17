1278f78133efa40eb64504892fbcb7b0d443ed9f
ColinChan520 <104440913+ColinChan520@users.noreply.github.com>
2024-05-17 11:33:04 +0930

    Create a bottom information bar to complete the basic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,8 +11,17 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout></Layout>} />
-        <Route path="/search" element={<>Search Page</>} />
+        <Route path="/" element={
+        <Layout>
+          <p>Home Page</p>
+        </Layout>
+      } 
+      />
+        <Route path="/search" element={
+          <Layout>
+          <p>Search Page</p>
+        </Layout>
+        } />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
