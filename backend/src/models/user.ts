393cdc9d02294502f923c794182bdba7c8ff82ef
ColinChan520 <104440913+ColinChan520@users.noreply.github.com>
2024-05-17 14:34:04 +0930

    Create a middleware between the backend and mongodb

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from "bcryptjs";
 
 export type UserType = {
     _id: string;
@@ -13,6 +14,13 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+    if(this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 8);
+    }
+    next();
 });
 
 const User = mongoose.model<UserType>('User', userSchema);
