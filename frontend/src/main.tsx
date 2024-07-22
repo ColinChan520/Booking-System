62feb7eb7b6b5c7c5ebf8b0e17019f72b60b41c3
ColinChan520 <Colinchan233@gmail.com>
2024-07-22 11:01:26 +0930

    Add user registration success message

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
         <App />
-     <QueryClientProvider
-  </React.StrictMode>,
+    </QueryClientProvider>
+  </React.StrictMode>
 )
+333
