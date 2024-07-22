833826e2736d07b967f2863ad850dc0167d22b2e
ColinChan520 <Colinchan233@gmail.com>
2024-07-22 11:28:39 +0930

    fix: Correct form validation for first and last name inputs in user registration

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -6,8 +6,8 @@ import { check, validationResult } from "express-validator";
 const router = express.Router();
 
 router.post("/register", [
-    check("firstName", "First name is required").not().isString(),
-    check("lastName", "Last name is required").not().isString(),
+    check("firstName", "First name is required").isString(),
+    check("lastName", "Last name is required").isString(),
     check("email", "Email is required").isEmail(),
     check("password", "Password with 6 or more characters required").isLength({min: 6}),
 ], async (req: Request, res: Response) => {
