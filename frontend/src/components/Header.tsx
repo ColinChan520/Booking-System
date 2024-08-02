1117f306773c65d627b1a901fec8b90c6bfdc40a
ColinChan520 <Colinchan233@gmail.com>
2024-08-02 12:53:59 +0930

    refactor: Improve header links styling and add hover effects

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,8 +13,8 @@ const Header = () => {
                 </span>
                 <span className="flex space-x-2">
                     {isLoggedIn ? (<> 
-                    <Link to="/my-bookings">My Bookings</Link>
-                    <Link to="/my-hotels">My Hotels</Link>
+                    <Link className="flex items-center text-white px-3 font-bold hover:bg-blue-600" to="/my-bookings">My Bookings</Link>
+                    <Link className="flex items-center text-white px-3 font-bold hover:bg-blue-600" to="/my-hotels">My Hotels</Link>
                     <SignOutButton />
                     </> ):  (                   
                     <Link to="/sign-in" className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-grey-100 hover:text-green-500">
