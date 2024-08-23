4b7c74588b270746253b3009c4018b5d19e59ff2
ColinChan520 <Colinchan233@gmail.com>
2024-08-23 16:15:49 +0930

    feat: Add link to registration page in Sign In form

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,7 +1,7 @@
 import {useForm} from "react-hook-form";
 import { useMutation, useQueryClient } from "react-query";
 import { useAppContext } from "../contexts/AppContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import * as apiClient from "../api-client";
 
 export type SignInFormData = {
@@ -58,7 +58,10 @@ const SignIn = () => {
                         {errors.password && <span className="text-red-500">{errors.password.message}</span>}
                     </label>
 
-                    <span>
+                    <span className = "flex items-center justify-between">
+                        <span className = "text-sm">
+                            Not registered?<Link className="underline" to="/register">Create an account here</Link>                     
+                        </span>
                         <button 
                         type="submit"
                         className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">LogIn</button>
